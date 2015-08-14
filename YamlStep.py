24cfcc0da0922a0ab057abfcee29f2e9e116daa6
junekeepgoing <1612213838@qq.com>
2015-08-14 20:30:04 +0800

    yamlstep

--- YamlStep.py
+++ YamlStep.py
@@ -18,6 +18,23 @@ class YamlStep():
         if self.expr_lines:
             for expr_line in self.expr_lines:
                 if "=" not in expr_line:
-                    YamlHelper.var_expr(variables, expr_line.strip())
+                    print YamlHelper.var_expr(variables, expr_line.strip())
                 else:
                     self.add_var(variables, expr_line)
+
+
+if __name__ == "__main__":
+    variables = {'Username': 'kentz', 
+    'A': 'TEST AAAAAA', 
+    'B': 'TEST AAAAAA/password', 
+    'Version': 'v2', 
+    'param3': 'TEST AAAAAA/0123456789/asjdlkfjalk', 
+    'param2': 'TEST AAAAAA/0123456789', 
+    'param1': '1234567890', 
+    'Password': '123456'}
+# - user/login/${SessionID}
+# - user/login/${GetSession kentz}
+# - user/login/${SessionID}/${Func param}
+# - user/login/
+    pre = YamlStep(["/user/login/${single_execute user_login kentz phone e10adc3949ba59abbe56e057f20f883e}"])
+    pre.execute(variables)
